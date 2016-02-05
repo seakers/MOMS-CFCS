9752ab52c5dc7f67f2b5717061f88f0b063457ca
SEAKLAB <seak@cornell.edu>
2016-02-05 18:00:02 -0500

    Added outputs to accumulateMunkresMatch to help sort out the common stuff in the matchs. Fixed a problem in countDiffs where the cluster sizes were deduced from the number of clusters merged which caused issues when passing in, say matchings.

--- accumulateMunkresMatch.m
+++ accumulateMunkresMatch.m
@@ -1,6 +1,8 @@
-function [connectivity]=accumulateMunkresMatch(clusters, locations, matching)
+function [connectivity, classMembership,matchingIndx]=accumulateMunkresMatch(clusters, locations, matching)
 totalEdges=sum(cellfun(@(x) sum(x>0),matching(:,3)),1);
 connectivity=nan(totalEdges,3);
+matchingIndx=nan(totalEdges,1);
+classMembership=nan(totalEdges,2);
 currentStart=1;
 
 for indx=1:size(matching,1)
@@ -9,6 +11,9 @@ for indx=1:size(matching,1)
     dist=sqrt(sum((y1-y2).^2,2));
     
     thisSprseSubMat=[cls1Used,cls2Used,dist];
-    connectivity(currentStart:(currentStart-1+size(thisSprseSubMat,1)),:)=thisSprseSubMat;
+    thisSubset=currentStart:(currentStart-1+size(thisSprseSubMat,1));
+    matchingIndx(thisSubset)=repmat(indx,size(cls1Used));
+    classMembership(thisSubset,:)=[repmat(matching{indx,1},size(cls1Used)),repmat(matching{indx,2},size(cls2Used))];
+    connectivity(thisSubset,:)=thisSprseSubMat;
     currentStart=currentStart+size(thisSprseSubMat,1);
 end
--- countDiffs.m
+++ countDiffs.m
@@ -1,10 +1,12 @@
 function diffsGrouping=countDiffs(diffFeat,clsMergeSz,sortEdgesByWeight)
+%% calculate unique identifiers.
 sortMergeSz=sort(clsMergeSz,2);
-radix=size(clsMergeSz,1)+1;
+radix=max(max(clsMergeSz))+1;
 UID=sum(sortMergeSz.*repmat([radix,1],size(sortMergeSz,1),1),2); % note size(y,1) is # of merge operations
-UIDfeat=bi2deSubstitute(diffFeat);
+UIDfeat=bi2deSubstitute(diffFeat); %Fails if have anything not {0,1}^n. Unique integer for each feature pattern to ease exact matching.
 numDigit=size(diffFeat,2);
 
+%%Buggy, don't want to quite figure out how right now as it's just display order.
 [usedFeats,featsSortIndx]=unique(UID);
 meanDist=NaN(size(usedFeats,1),1);
 featsSortMST=sortEdgesByWeight(featsSortIndx);
@@ -30,7 +32,7 @@ for merge=sortUsedFeats'
     firstDigit=mod(merge,radix); % digits of the merge pattern (cluster sizes to merge)
     secondDigit=(merge-firstDigit)/radix;
     diffsGrouping{cnt,1}=[firstDigit,secondDigit];
-    diffsGrouping{cnt,2}=de2biSubstitute(featSets,numDigit);
+    diffsGrouping{cnt,2}=de2biSubstitute(featSets,numDigit); %TODO reverse lookup in diffFeats. Tricky with all this sorting.
     diffsGrouping{cnt,3}=sortCnts;
     
     cnt=cnt+1;
