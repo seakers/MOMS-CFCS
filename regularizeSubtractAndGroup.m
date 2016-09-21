cf34061ba9a5544230e67d598b7d94b38ffbfaa3
Nathan Knerr <importN@hotmail.com>
2016-09-21 19:23:27 -0400

    Added regularizeSubtrctAndGroup, which makes the returns from the functions normal in that the leading nonzero element of the diff is positive by multiplying by -1 appropriately.

---
+++ regularizeSubtractAndGroup.m
@@ -0,0 +1,29 @@
+function diffsGrouping=regularizeSubtractAndGroup(subtractFeatures, varargin)
+   %%assuming features feature vectors of numbers across the rows
+   %%multiplies features by -1 to make the leading (1st nonzero) element
+   %%positive
+   %%then groups into identical features and collapses to unique elements
+   %%with counts.
+   %%optional argument for a callback to be callled after regularizing the
+   %%vectors but before grouping into unique elements (usually for rounding
+   %%out numerical errors)
+   
+   if nargin==2
+       postRegularCallback=varargin{1};
+   elseif nargin~=1
+       error('regularizeSubtractAndGroup takes 1 or 2 parameters');
+   end
+   
+   [row, col]=find(subtractFeatures~=0);
+   firstNonZeroCol=accumarray(row, col,[],@min,1);
+   needFix=logical(subtractFeatures(sub2ind(size(subtractFeatures),1:length(firstNonZeroCol),firstNonZeroCol'))<0); % NEVER FOR LOOP!!!!
+   subtractFeatures(needFix,:)=-subtractFeatures(needFix,:);
+   
+   if nargin==2
+       subtractFeatures=postRegularCallback(subtractFeatures);
+   end
+   
+   [uniqFeat, uniqIndxs, invUniqIndxs]=unique(subtractFeatures,'rows');
+   numElem=accumarray(invUniqIndxs,1); % number of each unique element. Fascinating function. Can also group into cell arrays (see example).
+   diffsGrouping={uniqIndxs, invUniqIndxs, uniqFeat, numElem}; % subtractFeatures[return[1]]->uniqueElements, inverse of <-, unique elements, number of each unique element
+   return
