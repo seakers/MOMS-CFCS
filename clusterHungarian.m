378a663ab5fc6185cd2c5bf0bf2559f0ac40aec0
Nathan Knerr <importN@hotmail.com>
2016-11-08 14:03:52 -0500

    Added repeated re-matchign to get larger from smaller. Works until the sets mismatch swaps.

--- clusterHungarian.m
+++ clusterHungarian.m
@@ -1,15 +1,60 @@
 function [matching]=clusterHungarian(clsPairs,distConn, varargin)
 matching=cell(size(clsPairs,1),3);
 
+
+% the code quality is so terrible due to my hacking I could practically
+% vomit.
 if(length(distConn)~=size(clsPairs,1))
     error('input size mismatch. clsPairs should corespond to distances in distConn');
 end
 
 disp([num2str(size(clsPairs,1)),' connections']);
+center=varargin{1};
+
 for indx=1:size(distConn,1)
     thisDist=distConn{indx};
     wrkCls=clsPairs(indx,1);jobCls=clsPairs(indx,2);
     
+    thisCenter=center{indx};
+
+    [wrkCls,jobCls,assign, thisDist, ~]=partialMatchOne(thisDist, thisCenter, wrkCls, jobCls);
+    if wrkCls==clsPairs(indx,1)
+        thisCenterL=thisCenter{1};
+        thisCenterR=thisCenter{2};
+    else
+        thisCenterL=thisCenter{2};
+        thisCenterR=thisCenter{1};
+    end
+    
+%% rematching for larger sets
+%     toAssign=(assign==0);
+%     while any(toAssign)
+%         [newWrkCls,~,newAssign]=partialMatchOne(thisDist(toAssign,:), {thisCenterL(toAssign), thisCenterR}, wrkCls, jobCls); % assert: thisDist should never be fat when called. Will eventually get to where should be fat and then have to index 2nd indexs...but then would have finished last iteration.
+%         toAssignIndxs=find(toAssign);
+%         if wrkCls~=newWrkCls % had more jobs than workers and partialMatchOne has transposed.
+%             jobIndxs=find(newAssign);
+%             [~,newWorkIndxs]=sort(newAssign(jobIndxs));
+%             workerIndxs=toAssignIndxs(newAssign(newAssign>0));
+%             newAssign=zeros(length(toAssignIndxs),1);
+%             newAssign(workerIndxs)=jobIndxs;
+%             newAssign=jobIndxs(newWorkIndxs);
+%         end
+%         
+%         assign(toAssignIndxs(newAssign>0))=newAssign(newAssign>0);
+%         toAssign=(assign==0);
+%     end
+    
+    matching(indx,:)={wrkCls,jobCls,assign};
+end
+%assign is a row vector which satisfies
+%assign(cls1element)==assignedCls2element where cls1element are
+%elements of the clusters with labels adjacent{i,1} and
+%adjacent{i,2} respectively.
+return
+
+function [wrkCls,jobCls,assign, thisDist, centerDist]=partialMatchOne(thisDist, centerDistsCell, wrkCls, jobCls)
+% this whol thing could be cleaned up if kept consistent left/right instead
+% of job/wrk classes
     [wrkrs,jobs]=size(thisDist);
     setCenter=false;
     if(wrkrs<jobs) % insure more workers than jobs
@@ -23,19 +68,17 @@ for indx=1:size(distConn,1)
         thisDist=thisDist'; % thisDist is tall.
         
         if nargin>2
-            center=varargin{1};
-            thisCenter=center{indx};
+            thisCenter=centerDistsCell;
             centerDist=repmat(thisCenter{2},wrkrs-jobs,1)';
             setCenter=true;
         end
     end
     if nargin>2 && ~setCenter
-        center=varargin{1};
-        thisCenter=center{indx};
+        thisCenter=centerDistsCell;
         centerDist=repmat(thisCenter{1},1,wrkrs-jobs);
     end
     
-    disp([num2str(indx), '<- indx | perfect Match size ->', num2str(jobs)])
+%     disp([num2str(indx), '<- indx | perfect Match size ->', num2str(jobs)])
     
     %the mixer mixes up indicies when used inside the ().
     %the unmixer tells who became the input index when used inside the ().
@@ -54,19 +97,14 @@ for indx=1:size(distConn,1)
     assign(assign>jobs)=0;
     assign(assign>0)=jobMixer(assign(assign>0));
     assign=assign(wrkUnmixer);
+    thisDist=unmixDist;
     
     % assign is a vector the length of the workCls and has indicies 0
     % (unmatched) through number of jobcls elements with workcls which are
     % matchings between a given element in the jobclass to the elements in
     % workclass.
-    unmatched=(assign==0);
-    [~,toMatch]=min(unmixDist(unmatched,:),[],2);
-    assign(unmatched)=toMatch;
+%     unmatched=(assign==0);
+%     [~,toMatch]=min(unmixDist(unmatched,:),[],2);
+%     assign(unmatched)=toMatch;
     
-    matching(indx,:)={wrkCls,jobCls,assign};
-end
-%assign is a row vector which satisfies
-%assign(cls1element)==assignedCls2element where cls1element are
-%elements of the clusters with labels adjacent{i,1} and
-%adjacent{i,2} respectively.
 return
